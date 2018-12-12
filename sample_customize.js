ce20ac08e8e97c7eca0e2a21762ed91929c7e680
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-12-12 19:06:58 +0900

    「ユーザー数」が500人以上の顧客情報については「見込み時期」をメッセージで表示する

--- sample_customize.js
+++ sample_customize.js
@@ -2,23 +2,13 @@
   'use strict';
 
   // レコード一覧イベントを取得
-  kintone.events.on('app.record.index.show', function(event) {
-    // 文字色の設定値
-    var fontColorRed = '#ff0000';
+  kintone.events.on('app.record.detail.show', function(event) {
+    // レコード情報を取得
+    var record = event.record;
 
-    // 「会社名」と「確度」の要素を取得
-    var elCustomer = kintone.app.getFieldElements('文字列__1行_'); // 会社名
-    var elAccuracy = kintone.app.getFieldElements('ラジオボタン'); // 確度
-
-    for (var i = 0; i < elAccuracy.length; i++) {
-      // レコード情報を取得
-      var record = event.records[i];
-
-      // 「確度」が"A"の場合「会社名」と「確度」の文字色を変更する
-      if (record['ラジオボタン']['value'] === 'A') {
-        elCustomer[i].style.color = fontColorRed;
-        elAccuracy[i].style.color = fontColorRed;
-      }
+    // ユーザー数が500人以上であればメッセージを表示
+    if (record['ユーザー数']['value'] >= 500) {
+      alert('ユーザー数500人以上の案件です\n【見込み期限】' + record['日付_0']['value']);
     }
   });
 })();
