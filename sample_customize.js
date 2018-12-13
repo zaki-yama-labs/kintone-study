92ff1d74eb8fcb156fb2319f1ce9d7dcf061f650
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-12-13 23:33:06 +0900

    kintone REST APIの利用

--- sample_customize.js
+++ sample_customize.js
@@ -2,13 +2,28 @@
   'use strict';
 
   // レコード一覧イベントを取得
-  kintone.events.on('app.record.detail.show', function(event) {
-    // レコード情報を取得
-    var record = event.record;
+  kintone.events.on('app.record.index.show', function(event) {
+    // アプリIDを取得
+    var appID = kintone.app.getId();
+    // ログインユーザ情報
+    var user = kintone.getLoginUser();
 
-    // ユーザー数が500人以上であればメッセージを表示
-    if (record['ユーザー数']['value'] >= 500) {
-      alert('ユーザー数500人以上の案件です\n【見込み期限】' + record['日付_0']['value']);
-    }
+    // クエリ文の設定
+    var qryInfo = 'ユーザー選択 in (LOGINUSER())';
+
+    // リクエストを行う
+    const data = {
+      app: appID,
+      query:qryInfo,
+    };
+    kintone.api('/k/v1/records', 'GET', data, (resp) => {
+      if (resp['records'].length > 0) {
+        var msg = '';
+        resp['records'].forEach(record => {
+          msg += `・ ${record['文字列__1行_']['value']}\n`;
+        });
+        alert(`${user.name}が担当している会社\n\n${msg}`);
+      }
+    });
   });
 })();
