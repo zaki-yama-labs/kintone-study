e9cdd91b30ec48e66ad8fc8909c15ac25d0a9c2e
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-12-16 17:11:55 +0900

    Delete addButtonOnRecordList.js

--- addButtonOnRecordList.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-
-  // レコード一覧画面の表示後イベント
-  kintone.events.on('app.record.index.show', (event) => {
-    // ここに具体的な処理の内容を記述する
-    kintone.app.getHeaderMenuSpaceElement().innerHTML = '<button id=\'my_index_button\'>一覧のボタン</button>';
-  });
-})();
