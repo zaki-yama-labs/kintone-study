5752f9e77dcd7ae3042cf879780a2f9739ef08a2
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-12-16 17:21:45 +0900

    02. イベントハンドラーを追加

--- 02_addButtonOnRecordList.js
+++ 02_addButtonOnRecordList.js
@@ -3,7 +3,18 @@
 
   // レコード一覧画面の表示後イベント
   kintone.events.on('app.record.index.show', (event) => {
-    // ここに具体的な処理の内容を記述する
-    kintone.app.getHeaderMenuSpaceElement().innerHTML = '<button id=\'my_index_button\'>一覧のボタン</button>';
+    // 増殖バグを防ぐ
+    if (document.getElementById('my_index_button') !== null) {
+      return;
+    }
+
+    const myIndexButton = document.createElement('button');
+    myIndexButton.id = 'my_index_button';
+    myIndexButton.innerHTML = '一覧のボタン';
+    myIndexButton.onclick = function() {
+      window.confirm('いま押しましたね？');
+    };
+
+    kintone.app.getHeaderMenuSpaceElement().appendChild(myIndexButton);
   });
 })();
