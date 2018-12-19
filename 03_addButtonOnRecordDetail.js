d6120b69a334618271a339ae4980ddb23c080b22
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-12-19 20:13:17 +0900

    03. tweak

--- 03_addButtonOnRecordDetail.js
+++ 03_addButtonOnRecordDetail.js
@@ -4,7 +4,7 @@
   // レコード詳細画面の表示後イベント
   kintone.events.on('app.record.detail.show', (event) => {
     // メニュー右側の空白部分にボタンを設置
-    const myIndexButton = document.getElementById('button');
+    const myIndexButton = document.createElement('button');
     myIndexButton.id = 'my_index_button';
     myIndexButton.innerHTML = 'メニュー部ボタン';
     myIndexButton.onclick = () => {
