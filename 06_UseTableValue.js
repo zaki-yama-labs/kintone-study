75529e94eabf44d294801be0d06d4b20b534a19e
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-01-15 01:03:04 +0900

    06. カテゴリ別の合計を算出する

--- 06_UseTableValue.js
+++ 06_UseTableValue.js
@@ -11,10 +11,29 @@
 
     // フィールドコード "order_items"のレコードオブジェクトを取得
     const tableRecords = event.record.order_items.value;
+    const subTotals = [];
 
-    // 2個目の "商品名" フィールドの値を取得
-    const secondItemName = tableRecords[1].value['商品名'].value;
+    tableRecords.forEach(record => {
+      // カテゴリ別に金額を集計
+      const category = record.value['カテゴリ'].value;
+      const unitPrice = record.value['単価'].value;
+      const amount = record.value['個数'].value;
 
-    alert(secondItemName);
+      if (!subTotals[category]) {
+        subTotals[category] = 0;
+      }
+
+      subTotals[category] += unitPrice * amount;
+    });
+
+    // 集計結果をスペースフィールドに表示する
+    const myTotalSpace = document.createElement('ul');
+    for (const category in subTotals) {
+      const categorySpace = document.createElement('li');
+      categorySpace.innerHTML = `${category}: ${subTotals[category]}円`;
+      myTotalSpace.appendChild(categorySpace);
+    }
+
+    kintone.app.record.getSpaceElement('sub_total_area').appendChild(myTotalSpace);
   });
 })();
